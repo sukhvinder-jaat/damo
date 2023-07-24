68b4c6deeac4da1016a7d84304ebf5181312f656
sukhvinder-jaat <monudhaka829@gmail.com>
2023-07-24 12:29:19 +0530

    add

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,31 +1,27 @@
 import React, { useState } from 'react';
-
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
-
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
-
   const handleAddTodo = () => {
     if (inputValue.trim() === '') return;
     setTodos([...todos, inputValue]);
     setInputValue('');
   };
-
   const handleRemoveTodo = (index) => {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
   };
-
+  
   return (
     <div>
       <h2 className=' text-danger'>To-Do List</h2>
       <div className=' d-flex align-items-center'>
         <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button onClick={handleAddTodo} className=' fs-5 border-0 bg-black text-white z-3'>Add</button>
+        <button onClick={handleAddTodo} className='ms-2 fs-5 border-0 bg-black text-white z-3'>Add</button>
       </div>
       <ul>
         {todos.map((todo, index) => (
